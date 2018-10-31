3fb5630a9ee27f85f948d09124a273934535dd3d
陈家铭 <755933630@qq.com>
2018-10-31 10:42:16 +0800

    模板二

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path(r'redirect', views.redirect, name='redirect'),
+    path(r'json', views.return_json, name='json'),
     path(r'', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,45 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, Http404
+from .models import Question
+from django.template import loader
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    least_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'least_question_list': least_question_list
+    }
+    print("查询到的查询集为：%s" % context)
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+
+def redirect(request):
+    return HttpResponseRedirect("/polls/json")
+
+
+def return_json(request):
+    return JsonResponse({"time": "morning"})
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist!")
+    return render(request, 'polls/detail.html', {'question': 'question'})
+
